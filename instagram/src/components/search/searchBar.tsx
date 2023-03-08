cc674241e92b31cdca5e05e54abd0bfd68b0ab8b
hsiehlin <1016634184@qq.com>
2023-03-08 00:14:07 -0500

    conflicts solved

--- instagram/src/components/search/searchBar.tsx
+++ instagram/src/components/search/searchBar.tsx
@@ -2,28 +2,6 @@ import { useState,useEffect } from "react";
 import SearchResultCard,{SearchResult} from './searchResultCard'
 import axios from "axios";
 
-/*
-const sampleCards : SearchResult[] = [
-    {userName:'user1',avatarURL:'./images/avatars/role11.jpg',userURL:'/1',isFollowing:true},
-    {userName:'user2',avatarURL:'./images/avatars/role12.jpg',userURL:'/2',isFollowing:true},
-    {userName:'user3',avatarURL:'./images/avatars/role13.jpg',userURL:'/',isFollowing:false},
-    {userName:'user4',avatarURL:'./images/avatars/role11.jpg',userURL:'/',isFollowing:true},
-    {userName:'user5',avatarURL:'./images/avatars/role12.jpg',userURL:'/',isFollowing:true},
-    {userName:'user6',avatarURL:'./images/avatars/role13.jpg',userURL:'/',isFollowing:false},
-    {userName:'user7',avatarURL:'./images/avatars/role11.jpg',userURL:'/',isFollowing:true},
-    {userName:'user8',avatarURL:'./images/avatars/role12.jpg',userURL:'/',isFollowing:true},
-    {userName:'user9',avatarURL:'./images/avatars/role13.jpg',userURL:'/',isFollowing:false},
-    {userName:'user10',avatarURL:'./images/avatars/role11.jpg',userURL:'/',isFollowing:true},
-    {userName:'user11',avatarURL:'./images/avatars/role12.jpg',userURL:'/',isFollowing:true},
-    {userName:'user12',avatarURL:'./images/avatars/role13.jpg',userURL:'/',isFollowing:false},
-    {userName:'user13',avatarURL:'./images/avatars/role11.jpg',userURL:'/',isFollowing:true},
-    {userName:'user14',avatarURL:'./images/avatars/role12.jpg',userURL:'/',isFollowing:true},
-    {userName:'user15',avatarURL:'./images/avatars/role13.jpg',userURL:'/',isFollowing:false},
-    {userName:'user16',avatarURL:'./images/avatars/role11.jpg',userURL:'/',isFollowing:true},
-    {userName:'user17',avatarURL:'./images/avatars/role12.jpg',userURL:'/',isFollowing:true},
-    {userName:'user18',avatarURL:'./images/avatars/role13.jpg',userURL:'/',isFollowing:false},
-];
-*/
 const sampleCards: SearchResult[] = [
     {userID: "1", userName:'user1',userDescription:"user description here aa aaaaaaaa aaaaaa aaaa aaaaaa",avatarURL:'./images/avatars/role11.jpg',userURL:'/1',isFollowing:true},
     {userID: "2",userName:'user2',userDescription:"user description here aaaaaaaaaaaaaaaaaaaaaaaaaa",avatarURL:'./images/avatars/role12.jpg',userURL:'/2',isFollowing:true},
@@ -45,10 +23,6 @@ const sampleCards: SearchResult[] = [
     {userID: "18",userName:'user18',userDescription:"user description here aaaaaaaaaaaaaaaaaaaaaaaaaa",avatarURL:'./images/avatars/role13.jpg',userURL:'/',isFollowing:false},
 ];
 
-export interface keyWordsBag{
-    keywords:string[];
-};
-
 export interface searchBarParas{
     contentBuffer : string;
     handleReturnBuffer : (arg0:string)=>void;
@@ -60,45 +34,45 @@ export default function SearchBar(props:searchBarParas){
     
     const handleReturnBuffer = props.handleReturnBuffer;
     
-    const handleKeyWordsConfirmed=async ()=>{
-        resultCards.length=0;
-        let keyWords:string[] = inputValue.trim().replace(/\s\s+/g, ' ').split(' ');
-        console.log("handle key words confirmed")
-        /** TO DO: send ajax
-        const formData = new FormData();
-        for(const keyWord of keyWords){
-            formData.append("keywords",keyWord);
-        }
-        await axios({
-            method : "post",
-            url : "./search",
-            data: formData,
-            headers : {"Content-Type" : "multipart/form-data"},
-        }).then(function (res){
-            let results = res.data;
-            if(Array.isArray(results)){
-                setResultCards(results);
+    useEffect(()=>{
+        const delayHandleSearchRequest = setTimeout(()=>{
+            resultCards.length=0;
+            let keyWords:string[] = inputValue.trim().replace(/\s\s+/g, ' ').split(' ');
+            console.log("handle key words confirmed")
+            /** TO DO: send ajax
+            const formData = new FormData();
+            for(const keyWord of keyWords){
+                formData.append("keywords",keyWord);
             }
-            else{//res.data is not array
-                throw new Error("incorrect reponse of search: not an array of results");
-            }
-        }).catch(function (err){
-            console.error(err);
-        });
-        */
-         
-    
-        sampleCards.map((result)=>{
-            const userName = result.userName;
-            
-            for(const keyword of keyWords){
-                if(userName.match(keyword)){
-                    setResultCards((resultCards)=>[...resultCards,result])
-                    break;
+            await axios({
+                method : "post",
+                url : "./search",
+                data: formData,
+                headers : {"Content-Type" : "multipart/form-data"},
+            }).then(function (res){
+                let results = res.data;
+                if(Array.isArray(results)){
+                    setResultCards(results);
                 }
-            }
-        });
-    };
+                else{//res.data is not array
+                    throw new Error("incorrect reponse of search: not an array of results");
+                }
+            }).catch(function (err){
+                console.error(err);
+            });
+            */
+            sampleCards.map((result)=>{
+                const userName = result.userName;
+                for(const keyword of keyWords){
+                    if(keyword !== "" && userName.match(keyword)){
+                        setResultCards((resultCards)=>[...resultCards,result])
+                        break;
+                    }
+                }
+            });
+        },500);
+        return ()=>clearTimeout(delayHandleSearchRequest);
+    },[inputValue]);
 
     console.log("rerender")
 
@@ -115,11 +89,10 @@ export default function SearchBar(props:searchBarParas){
                             setInputValue(event.target.value);
                             handleReturnBuffer(event.target.value);
                         }}
-                        onKeyDown={(event)=>{if(event.key==='Enter')handleKeyWordsConfirmed();}} 
                         placeholder="Search"
                         value = {inputValue} 
                         className="searchBox outline-none bg-transparent focus:border-tranparent"/>
-                <button type="button" className="searchButton" onClick={handleKeyWordsConfirmed}>Submit</button>
+                <button type="button" className="searchButton">Submit</button>
             </form>
             <div className="pt-6"></div>
             
@@ -136,3 +109,9 @@ export default function SearchBar(props:searchBarParas){
         </div>
     );
 }
+
+
+
+
+
+
--- instagram/src/components/search/searchResultCard.tsx
+++ instagram/src/components/search/searchResultCard.tsx
@@ -1,14 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-/*
-export interface SearchResult{
-    userName:string;
-    avatarURL:string;
-    mainPageURL:string;
-    isFollowed:boolean;
-};
-*/
 export interface SearchResult{
     userID : string;
     userName: string;
@@ -38,26 +30,7 @@ export default function SearchResultCard(props:SearchCardProps){
     return (
         
             <div className="flex flex-row h-16 w-full hover:bg-slate-100 p-1">
-                <div  className='h-16 w-4/6 no-underline'>
-                    <div className="flex flex-row p-1 h-full w-full">
-                        <img src={result.avatarURL} alt={result.userName} className="w-[40px] h-[40px] rounded-full hover:scale-105"/>
-                        <div className="flex flex-col h-full w-[calc(100%-40px)]" >
-                            <p className="h-1/2 w-full pl-3 pb-0 mb-0 font-semibold text-sm overflow-hidden">{result.userName}</p>
-                            <p className="h-1/2 w-full pl-3 pb-3 font-light text-sm overflow-hidden">{result.userDescription}</p>
-                        </div>
-                    </div>
-                </div>
-                {isFollowed ? (
-                    <button className="w-2/6 border-transparent bg-transparent text-gray-500 text-sm font-semibold" onClick={handleFollowingClicked}>Following</button> 
-                ):(
-                    <button className="w-2/6 border-transparent bg-transparent text-blue-600 text-sm font-semibold" onClick={handleFollowClicked}>Follow</button>
-                )}
-            </div>
-       
-    );
-
-    /**add this after introducing route
-     * <Link to={result.userURL}  className='h-16 w-4/6 no-underline'>
+                <Link to={`p/${result.userName}`}  className='h-16 w-4/6 no-underline cursor-pointer'>
                     <div className="flex flex-row p-1 h-full w-full">
                         <img src={result.avatarURL} alt={result.userName} className="w-[40px] h-[40px] rounded-full hover:scale-105"/>
                         <div className="flex flex-col h-full w-[calc(100%-40px)]" >
@@ -66,5 +39,12 @@ export default function SearchResultCard(props:SearchCardProps){
                         </div>
                     </div>
                 </Link>
-     */
+                {isFollowed ? (
+                    <button className="w-2/6 border-transparent bg-transparent text-gray-500 text-sm font-semibold" onClick={handleFollowingClicked}>Following</button> 
+                ):(
+                    <button className="w-2/6 border-transparent bg-transparent text-blue-600 text-sm font-semibold" onClick={handleFollowClicked}>Follow</button>
+                )}
+            </div>
+       
+    );
 }
